338e23eac1783c2e1dea73cb03427562364fb0ab
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2022-01-08 11:23:05 -0300

    login no app

--- app/screens/login.screen.tsx
+++ app/screens/login.screen.tsx
@@ -1,22 +1,40 @@
 import React, {useState} from 'react';
-import {StyleSheet, View} from 'react-native';
+import {Alert, StyleSheet, View} from 'react-native';
 import {Text} from 'react-native-elements';
 import {Button} from 'react-native-elements/dist/buttons/Button';
 import {Input} from 'react-native-elements/dist/input/Input';
+import {ActivityIndicator} from 'react-native-paper';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import usuarioService from '../services/usuario.service';
 import styles from '../style/main-style';
 
 const LoginScreen = ({navigation}) => {
   const [email, setEmail] = useState(null);
   const [password, setPassword] = useState(null);
+  const [isLoading, setLoading] = useState(false);
 
   const entrar = () => {
-    //   navigation.navigate('Home');
-    // desabilita o botão voltar resetando a pilha
-    navigation.reset({
-      index: 0,
-      routes: [{name: 'Principal'}],
-    });
+    let data = {
+      username: email,
+      password: password,
+    };
+
+    usuarioService
+      .login(data)
+      .then(response => {
+        setLoading(false);
+
+        //   navigation.navigate('Home');
+        // desabilita o botão voltar resetando a pilha
+        navigation.reset({
+          index: 0,
+          routes: [{name: 'Principal'}],
+        });
+      })
+      .catch(error => {
+        setLoading(false);
+        Alert.alert('Erro', 'Usuário ou Senha inválido');
+      });
   };
 
   const cadastrar = () => {
@@ -39,12 +57,16 @@ const LoginScreen = ({navigation}) => {
         secureTextEntry={true}
       />
 
-      <Button
-        icon={<Icon name="arrow-right" size={15} color="black" />}
-        title=" Entrar"
-        buttonStyle={buttonStyles.button}
-        onPress={() => entrar()}
-      />
+      {isLoading && <ActivityIndicator />}
+
+      {!isLoading && (
+        <Button
+          icon={<Icon name="arrow-right" size={15} color="black" />}
+          title=" Entrar"
+          buttonStyle={buttonStyles.button}
+          onPress={() => entrar()}
+        />
+      )}
 
       <Button
         icon={<Icon name="user" size={15} color="black" />}
--- app/services/usuario.service.ts
+++ app/services/usuario.service.ts
@@ -18,6 +18,24 @@ class UsuarioService {
         return Promise.reject(error);
       });
   }
+
+  async login(data: any) {
+    return axios({
+      url: 'http://192.168.1.106:3000/usuarios/login',
+      method: 'POST',
+      timeout: 5000,
+      data: data,
+      headers: {
+        Accept: 'application/json',
+      },
+    })
+      .then(response => {
+        return Promise.resolve(response);
+      })
+      .catch(error => {
+        return Promise.reject(error);
+      });
+  }
 }
 
 const usuarioService = new UsuarioService();
